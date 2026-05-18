fb62f67ac549024047211c0a7328f91290578de6
Rajan Mahato <rajancdy14@gmail.com>
2026-05-18 18:18:53 +0545

    Delete Dice Roll directory

--- Dice Roll/script.js
+++
@@ -1,9 +0,0 @@
-function rolldice(){
-    let max=6;
-    let min=1;
-    let rand=Math.floor(Math.random()*max)+min;
-    resultlbl.innerHTML=`
-        <h1> You dice is ${rand} </h1>
-        <img src="image/${rand}.png" width="120">`;  
-}
-    
