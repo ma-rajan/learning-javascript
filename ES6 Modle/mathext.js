4e7c9e9e21c6d4fe7a28b1e6e05529c0e907b617
rajan <rajancdy14@gmail.com>
2026-05-21 18:57:28 +0545

    Use another file feature using extend and import keyword

---
+++ ES6 Modle/mathext.js
@@ -0,0 +1,7 @@
+export const Pi=3.14;
+export function circumference(radius){
+    return 2*Pi*radius;
+}
+export function area(radius){
+    return Pi*Math.pow(radius,2);
+}
---
+++ ES6 Modle/script.js
@@ -0,0 +1,6 @@
+import {Pi,circumference,area} from './mathext.js';
+
+let c=circumference(Pi);
+let a=area(Pi);
+console.log("Circumference of circle is "+c);
+console.log("Area of circle is : "+a);
