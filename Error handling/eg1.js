b371a7c657534149a58566c33997d5c592208a43
rajan <rajancdy14@gmail.com>
2026-05-21 18:55:43 +0545

    Error handling in js

---
+++ Error handling/eg1.js
@@ -0,0 +1,9 @@
+try{
+    let a=12;
+    let b=0;
+    let result=a/b;
+    console.log("Divid of number is : "+result);
+}
+catch(error){
+    console.log(`Number cannot divide with zero`);
+}
---
+++ Error handling/eg2.js
@@ -0,0 +1,7 @@
+try{
+    let a=[4,43,6,8,9,4];
+    console.log(a[9]);
+}
+catch(error){
+    console.log(error);
+}
